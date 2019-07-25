21ca346fa28a3428e798ceeb0f7853704519a020
Zirbo <silvano.ferrari@ymail.com>
2019-07-25 14:34:37 +0200

    removed confusing comments

--- src/IPC.hpp
+++ src/IPC.hpp
@@ -3,13 +3,10 @@
 
 struct Particle {
     double x[3], v[3], F[3];
-    // TODO: when all the sim is working, remove the forces from here and only keep the IPC effective forces
 };
 struct IPCbase {
   Particle ipcCenter;
   char type;
-  // unfortunately I need the number for managing the lists...
-  // there's probably another way but I don't see it now, and anyway this is cheap
   int number;
 };
 
