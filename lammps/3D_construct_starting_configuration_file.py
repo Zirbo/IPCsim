47895cda841b90ecb9c3f7ee9daa4db7b7d667be
Zirbo <silvano.ferrari@ymail.com>
2019-10-15 16:39:31 +0200

    LAMMPS: adapted startingstate creator for the RIGID package

--- lammps/3D_construct_starting_configuration_file.py
+++ lammps/3D_construct_starting_configuration_file.py
@@ -23,40 +23,46 @@ print(args)
 outputFile.write("# 3D starting configuration for LAMMPS created with a script available at")
 outputFile.write("# https://gitlab.com/catnat/ipc_brownian_motion")
 outputFile.write("\n")
-outputFile.write("\n" + str(3*args.nIPCs).rjust(10) + " atoms")
-outputFile.write("\n" + str(2*args.nIPCs).rjust(10) + " bonds")
-outputFile.write("\n" + str(0).rjust(10) + " angles")
-outputFile.write("\n" + str(0).rjust(10) + " dihedrals")
+outputFile.write("\n" + str(3*args.nIPCs).rjust(16) + " atoms")
+outputFile.write("\n" + str(2*args.nIPCs).rjust(16) + " bonds")
+outputFile.write("\n" + str(  args.nIPCs).rjust(16) + " angles")
 outputFile.write("\n")
 
-outputFile.write("\n" + str(2).rjust(10) + " atom types")
-outputFile.write("\n" + str(1).rjust(10) + " bond types")
-outputFile.write("\n" + str(0).rjust(10) + " angle types")
-outputFile.write("\n" + str(0).rjust(10) + " dihedral types")
+outputFile.write("\n" + str(2).rjust(16) + " atom types")
+outputFile.write("\n" + str(1).rjust(16) + " bond types")
+outputFile.write("\n" + str(1).rjust(16) + " angle types")
 outputFile.write("\n")
 
-outputFile.write("\n" + str(0.0).rjust(10) + '{:3.8f}'.format(args.side).rjust(16) + " xlo xhi")
-outputFile.write("\n" + str(0.0).rjust(10) + '{:3.8f}'.format(args.side).rjust(16) + " ylo yhi")
-outputFile.write("\n" + str(0.0).rjust(10) + '{:3.8f}'.format(args.side).rjust(16) + " zlo zhi")
+outputFile.write("\n" + '{:3.8f}'.format(0.0).rjust(16) +
+                        '{:3.8f}'.format(args.side).rjust(16) + "     xlo xhi")
+outputFile.write("\n" + '{:3.8f}'.format(0.0).rjust(16) +
+                        '{:3.8f}'.format(args.side).rjust(16) + "     ylo yhi")
+outputFile.write("\n" + '{:3.8f}'.format(0.0).rjust(16) +
+                        '{:3.8f}'.format(args.side).rjust(16) + "     zlo zhi")
+
 outputFile.write("\n")
 outputFile.write("\nMasses")
+outputFile.write("\n#  atomtype, mass")
 outputFile.write("\n" + str(1).rjust(10) + str(2.0).rjust(10))
 outputFile.write("\n" + str(2).rjust(10) + str(0.5).rjust(10))
+
 outputFile.write("\n")
 outputFile.write("\nAtoms")
-outputFile.write("\n")
+outputFile.write("\n#  atom-ID mol-ID atom-type charge    x               y               z")
 for iz in range(0, args.nIPCsSide):
     for iy in range(0, args.nIPCsSide):
         for ix in range(0, args.nIPCsSide):
             # ipc center
-            i = 3*(ix + iy*args.nIPCsSide + iz*args.nIPCsPlane)
+            i = 1 + 3*(ix + iy*args.nIPCsSide + iz*args.nIPCsPlane)
             x = (0.5 + ix + 0.1*ranf())*args.side
             y = (0.5 + iy + 0.1*ranf())*args.side
             z = (0.5 + iz + 0.1*ranf())*args.side
-            outputFile.write("\n" + str(i).rjust(10) + str(1).rjust(10) +
+            outputFile.write("\n" + str(i).rjust(10) +
+                  str(1).rjust(10) +
+                  str(0.0).rjust(10) +
                  '{:3.8f}'.format(x).rjust(16) +
                  '{:3.8f}'.format(y).rjust(16) +
-                 '{:3.8f}'.format(z).rjust(16))
+                 '{:3.8f}'.format(z).rjust(16) )
             # create random unit vector
             px = ranf() 
             py = ranf()
@@ -66,13 +72,39 @@ for iz in range(0, args.nIPCsSide):
             py /= mod
             pz /= mod
             # patches
-            outputFile.write("\n" + str(i+1).rjust(10) + str(2).rjust(10) +
+            outputFile.write("\n" + str(i+1).rjust(10) +
+                  str(2).rjust(10) +
+                  str(0.0).rjust(10) +
                  '{:3.8f}'.format(x + args.ecc*px).rjust(16) +
                  '{:3.8f}'.format(y + args.ecc*py).rjust(16) +
-                 '{:3.8f}'.format(z + args.ecc*pz).rjust(16))
-            outputFile.write("\n" + str(i+2).rjust(10) + str(2).rjust(10) +
+                 '{:3.8f}'.format(z + args.ecc*pz).rjust(16) )
+            outputFile.write("\n" + str(i+2).rjust(10) +
+                  str(2).rjust(10) +
+                  str(0.0).rjust(10) +
                  '{:3.8f}'.format(x - args.ecc*px).rjust(16) +
                  '{:3.8f}'.format(y - args.ecc*py).rjust(16) +
-                 '{:3.8f}'.format(z - args.ecc*pz).rjust(16))
+                 '{:3.8f}'.format(z - args.ecc*pz).rjust(16) )
 
+outputFile.write("\n")
+outputFile.write("\nBonds")
+outputFile.write("\n#  ID bond-type atom-1 atom-2")
+for i in range(args.nIPCs):
+    IDcenter = 3*i + 1
+    IDpatch1 = 3*i + 2
+    IDpatch2 = 3*i + 3
+    outputFile.write("\n" + str(2*i+1).rjust(10) + str(1).rjust(10) +
+                            str(IDcenter).rjust(10) + str(IDpatch1).rjust(10) )
+    outputFile.write("\n" + str(2*i+2).rjust(10) + str(1).rjust(10) +
+                            str(IDcenter).rjust(10) + str(IDpatch2).rjust(10) )
+
+outputFile.write("\n")
+outputFile.write("\nAngles")
+outputFile.write("\n#  ID    angle-type atom-1 atom-2 atom-3  (atom-2 is the center atom in angle)")
+for i in range(args.nIPCs):
+    IDcenter = 3*i + 1
+    IDpatch1 = 3*i + 2
+    IDpatch2 = 3*i + 3
+    outputFile.write("\n" + str(i+1).rjust(10) + str(1).rjust(10) +
+                            str(IDpatch1).rjust(10) + str(IDcenter).rjust(10) +
+                            str(IDpatch2).rjust(10) )
 outputFile.write("\n")
