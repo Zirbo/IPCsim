6d0a872fed9e7b96760df39636c34186b995e1e1
Zirbo <silvano.ferrari@ymail.com>
2019-11-21 16:55:42 +0100

    script for angular plots of the w_ij

---
+++ toyModelWithScaledOmegas/computeAngularPlotsAndScaledCoefficients.py
@@ -0,0 +1,154 @@
+#! /usr/bin/python3
+
+import argparse
+from math import sqrt, pi, fabs, cos
+from math import sin as sen
+
+helpString = """
+"""
+
+parser = argparse.ArgumentParser(description=helpString)
+parser.add_argument('mode', metavar='m', type=str, help='usage mode')
+parser.add_argument('delta', metavar='d', type=float, help='interaction range minus diameter')
+parser.add_argument('ecc', metavar='e', type=float, help='eccentricity')
+parser.add_argument('eBB', metavar='eBB', type=float, help='')
+parser.add_argument('eBS', metavar='eBS', type=float, help='')
+parser.add_argument('eSS', metavar='eSS', type=float, help='')
+parser.add_argument('emin', metavar='emin', type=float, help='')
+##parser.add_argument('', metavar='', type=int, help='')
+args = parser.parse_args()
+
+usageMode = args.mode
+ecc = args.ecc
+delta = args.delta
+eBB = args.eBB
+eBS = args.eBS
+eSS = args.eSS
+emin = args.emin
+
+HSradius = 0.5
+HSdiameter = 1.0
+bigRadius = HSradius + delta/2
+patchRadius = bigRadius - ecc
+
+def computeOmega(Ra, Rb, rab):
+  # BKL paper, formula 18
+  if ( rab > Ra+Rb ):
+    return 0.
+  elif ( rab <= fabs(Ra-Rb) ):
+    return (min(Ra,Rb)/HSradius)**3;
+  else:
+    tempSum = (Ra**2-Rb**2)/(2.*rab)
+    return (0.25/HSradius**3) * (
+               (2.*Ra+tempSum+rab/2.)*(Ra-tempSum-rab/2.)**2 +
+               (2.*Rb-tempSum+rab/2.)*(Rb+tempSum-rab/2.)**2 )
+
+def computePotentialVerticalParticle(theta):
+  rP1P2 = sqrt( (HSdiameter + ecc*sen(theta))**2 + (ecc*(1 - cos(theta)))**2 )
+  rP1B2 = sqrt( ecc*2 + HSdiameter**2 )
+  rP1Q2 = sqrt( (HSdiameter - ecc*sen(theta))**2 + (ecc*(1 + cos(theta)))**2 )
+  rB1P2 = sqrt( (HSdiameter + ecc*sen(theta))**2 + (ecc*cos(theta))**2 )
+  rB1B2 = HSdiameter
+  rB1Q2 = sqrt( (HSdiameter - ecc*sen(theta))**2 + (ecc*cos(theta))**2 )
+  rQ1P2 = sqrt( (HSdiameter + ecc*sen(theta))**2 + (ecc*(1 + cos(theta)))**2 )
+  rQ1B2 = sqrt( ecc*2 + HSdiameter**2 )
+  rQ1Q2 = sqrt( (HSdiameter - ecc*sen(theta))**2 + (ecc*(1 - cos(theta)))**2 )
+
+  wBB = computeOmega(bigRadius,   bigRadius,   rB1B2)
+  wBS = computeOmega(bigRadius,   patchRadius, rB1Q2)
+  
+  V = (
+        eBB*( computeOmega(bigRadius,   bigRadius,   rB1B2) ) +
+        eBS*( computeOmega(patchRadius, bigRadius,   rP1B2) +
+              computeOmega(bigRadius,   patchRadius, rB1P2) +
+              computeOmega(bigRadius,   patchRadius, rB1Q2) +
+              computeOmega(patchRadius, bigRadius,   rQ1B2) ) +
+        eSS*( computeOmega(patchRadius, patchRadius, rP1P2) +
+              computeOmega(patchRadius, patchRadius, rP1Q2) +
+              computeOmega(patchRadius, patchRadius, rQ1P2) +
+              computeOmega(patchRadius, patchRadius, rQ1Q2) )
+      )
+  return V, wBB, wBS
+
+def computePotentialHorizontalParticle(theta):
+  rP1P2 = sqrt( (HSdiameter + ecc*(1 + sen(theta)))**2 + (ecc*cos(theta))**2 )
+  rP1B2 = ecc + HSdiameter
+  rP1Q2 = sqrt( (HSdiameter + ecc*(1 - sen(theta)))**2 + (ecc*cos(theta))**2 )
+  rB1P2 = sqrt( (HSdiameter + ecc*sen(theta))**2 + (ecc*cos(theta))**2 )
+  rB1B2 = HSdiameter
+  rB1Q2 = sqrt( (HSdiameter - ecc*sen(theta))**2 + (ecc*cos(theta))**2 )
+  rQ1P2 = sqrt( (HSdiameter - ecc*(1 - sen(theta)))**2 + (ecc*cos(theta))**2 )
+  rQ1B2 = ecc + HSdiameter
+  rQ1Q2 = sqrt( (HSdiameter - ecc*(1 + sen(theta)))**2 + (ecc*cos(theta))**2 )
+
+  wSS = computeOmega(patchRadius, patchRadius, rQ1Q2)
+  
+  V = (
+        eBB*( computeOmega(bigRadius,   bigRadius,   rB1B2) ) +
+        eBS*( computeOmega(patchRadius, bigRadius,   rP1B2) +
+              computeOmega(bigRadius,   patchRadius, rB1P2) +
+              computeOmega(bigRadius,   patchRadius, rB1Q2) +
+              computeOmega(patchRadius, bigRadius,   rQ1B2) ) +
+        eSS*( computeOmega(patchRadius, patchRadius, rP1P2) +
+              computeOmega(patchRadius, patchRadius, rP1Q2) +
+              computeOmega(patchRadius, patchRadius, rQ1P2) +
+              computeOmega(patchRadius, patchRadius, rQ1Q2) )
+      )
+  return V, wSS
+
+def computePotentials():
+  thetas = [ i*0.5*pi/20. for i in range(21) ]
+  Vh = []
+  Vv = []
+  wBB = []
+  wBS = []
+  wSS = []
+  for theta in thetas:
+    a, b, c = computePotentialVerticalParticle(theta)
+    d, e = computePotentialHorizontalParticle(theta)
+
+    Vh.append(d)
+    Vv.append(a)
+    wBB.append(b)
+    wBS.append(c)
+    wSS.append(e)
+
+  return Vv, Vh, wBB, wBS, wSS, thetas
+
+fBB = computeOmega(bigRadius, bigRadius, HSdiameter)
+fBS = computeOmega(bigRadius, patchRadius, HSdiameter - ecc)
+fSS = computeOmega(patchRadius, patchRadius, HSdiameter - 2*ecc)
+
+print("volumes at contact:")
+print( fBB, fBS, fSS)
+
+if usageMode == "include":
+  cBB = eBB / (fBB * emin)
+  cBS = eBS / (fBS * emin)
+  cSS = eSS / (fSS * emin)
+  print("scaled OUTPUT:")
+elif usageMode == "extract":
+  cBB = eBB * fBB / emin
+  cBS = eBS * fBS / emin
+  cSS = eSS * fSS / emin
+  print("multiplied OUTPUT:")
+else:
+  print("mode does not exist")
+  exit()
+
+
+print(str(cBB)[0:7].ljust(10) + str(cBS)[0:8].ljust(10) + str(cBS)[0:8].ljust(10))
+print(str(cSS)[0:7].ljust(10) + str(cSS)[0:7].ljust(10) + str(cSS)[0:7].ljust(10))
+
+#print('{0: 1.5e}    {1: 1.5e}    {1: 1.5e}'.format(cBB, cBS))
+#print('{0: 1.5e}    {0: 1.5e}    {0: 1.5e}'.format(cSS))
+
+Vv, Vh, wBB, wBS, wSS, theta = computePotentials()
+
+outputWns = open("wBB.txt", 'w')
+outputWss = open("wBBscaled.txt", 'w')
+outputPot = open("potential.txt", 'w')
+for Vvi, Vhi, wBBi, wBSi, wSSi, thetai in zip(Vv, Vh, wBB, wBS, wSS, theta):
+  outputWns.write(str(thetai).ljust(24) + str(wBBi).ljust(24) + str(wBSi).ljust(24) + str(wSSi).ljust(24) + "\n")
+  outputWss.write(str(thetai).ljust(24) + str(wBBi/fBB).ljust(24) + str(wBSi/fBS).ljust(24) + str(wSSi/fSS).ljust(24) + "\n")
+  outputPot.write(str(thetai).ljust(24) + str(Vvi).ljust(24) + str(Vhi).ljust(24) + "\n")
--- toyModelWithScaledOmegas/computeScaledCoefficients.py
+++ toyModelWithScaledOmegas/computeScaledCoefficients.py
@@ -7,20 +7,23 @@ helpString = """
 """
 
 parser = argparse.ArgumentParser(description=helpString)
+parser.add_argument('mode', metavar='m', type=str, help='usage mode')
 parser.add_argument('delta', metavar='d', type=float, help='interaction range minus diameter')
 parser.add_argument('ecc', metavar='e', type=float, help='eccentricity')
 parser.add_argument('eBB', metavar='eBB', type=float, help='')
 parser.add_argument('eBS', metavar='eBS', type=float, help='')
 parser.add_argument('eSS', metavar='eSS', type=float, help='')
+parser.add_argument('emin', metavar='emin', type=float, help='')
 ##parser.add_argument('', metavar='', type=int, help='')
 args = parser.parse_args()
 
+usageMode = args.mode
 ecc = args.ecc
 delta = args.delta
 eBB = args.eBB
 eBS = args.eBS
 eSS = args.eSS
-
+emin = args.emin
 
 HSradius = 0.5
 HSdiameter = 1.0
@@ -32,39 +35,38 @@ def computeOmega(Ra, Rb, rab):
   if ( rab > Ra+Rb ):
     return 0.
   elif ( rab <= fabs(Ra-Rb) ):
-    return (1./HSradius**3)*min(Ra,Rb)**3;
+    return (min(Ra,Rb)/HSradius)**3;
   else:
     tempSum = (Ra**2-Rb**2)/(2.*rab)
-    return (0.25/HSradius**3)*( (2.*Ra+tempSum+rab/2.)*(Ra-tempSum-rab/2.)**2 +
-                (2.*Rb-tempSum+rab/2.)*(Rb+tempSum-rab/2.)**2 )
-
+    return (0.25/HSradius**3) * (
+               (2.*Ra+tempSum+rab/2.)*(Ra-tempSum-rab/2.)**2 +
+               (2.*Rb-tempSum+rab/2.)*(Rb+tempSum-rab/2.)**2 )
 
 
 fBB = computeOmega(bigRadius, bigRadius, HSdiameter)
 fBS = computeOmega(bigRadius, patchRadius, HSdiameter - ecc)
 fSS = computeOmega(patchRadius, patchRadius, HSdiameter - 2*ecc)
 
-print ( fBB, fBS, fSS)
+print("volumes at contact:")
+print( fBB, fBS, fSS)
+
+if usageMode == "include":
+  cBB = eBB / (fBB * emin)
+  cBS = eBS / (fBS * emin)
+  cSS = eSS / (fSS * emin)
+  print("scaled OUTPUT:")
+elif usageMode == "extract":
+  cBB = eBB * fBB / emin
+  cBS = eBS * fBS / emin
+  cSS = eSS * fSS / emin
+  print("multiplied OUTPUT:")
+else:
+  print("mode does not exist")
+  exit()
 
 
-cBB = eBB * fBB
-cBS = eBS * fBS
-cSS = eSS * fSS
+print(str(cBB)[0:7].ljust(10) + str(cBS)[0:8].ljust(10) + str(cBS)[0:8].ljust(10))
+print(str(cSS)[0:7].ljust(10) + str(cSS)[0:7].ljust(10) + str(cSS)[0:7].ljust(10))
 
-print("scaled OUTPUT:\n")
-
-print("{:.6f}".format(cBB).rjust(10) + "\t" + "{:.6f}".format(cBS).rjust(10) + "\t" +"{:.6f}".format(cBS).rjust(10) + "\n")
-print("{:.6f}".format(cSS).rjust(10) + "\t" + "{:.6f}".format(cSS).rjust(10) + "\t" +"{:.6f}".format(cSS).rjust(10) + "\n")
-#print(str(cBB).rjust(10) + "\t" + str(cBS).rjust(10) + "\t" + str(cBS).rjust(10) + "\n")
-#print(str(cSS).rjust(10) + "\t" + str(cSS).rjust(10) + "\t" + str(cSS).rjust(10) + "\n")
-print("1.00000\n")
-
-
-cBB = eBB / fBB
-cBS = eBS / fBS
-cSS = eSS / fSS
-
-print("multiplied OUTPUT:\n")
-print(str(cBB).rjust(10) + "\t" + str(cBS).rjust(10) + "\t" + str(cBS).rjust(10) + "\n")
-print(str(cSS).rjust(10) + "\t" + str(cSS).rjust(10) + "\t" + str(cSS).rjust(10) + "\n")
-print("1.00000\n")
+#print('{0: 1.5e}    {1: 1.5e}    {1: 1.5e}'.format(cBB, cBS))
+#print('{0: 1.5e}    {0: 1.5e}    {0: 1.5e}'.format(cSS))
