e712039a5ddc45d159d6f12e9adfe33082a8e450
ferrasi1 <silvano.ferrari@ymail.com>
2019-12-07 17:50:30 +0100

    startingstate creator for the square plane of the FCC lattice of the 60n

---
+++ startingstate_creators/epitaxy_60n_square_plane.py
@@ -0,0 +1,102 @@
+#! /usr/bin/python3
+
+# writes a configuration with a single square plane of ipcs, and other ipcs above it.
+
+from math import cos, sin, sqrt, pi, floor
+from numpy.random import ranf as ran
+
+outputFile = open('startingstate.xyz','w')
+
+NPx = 14           # particles in a side
+NPy = 14           # particles in a side
+spacing = 1.2      # distance between fluid particles
+L   = 14.2         # scaling (box side)
+ecc = 0.16         # eccentricity
+
+
+ecc /= L
+cos30 = sqrt(3)*.5
+NFx = int(L/spacing)
+NFy = int(L/spacing)
+NFz = int(L/spacing) - 1
+spacing /= L
+
+def absolutePBC(z):
+    return z - floor(z)
+
+tempN = 3*(NPx*NPy + NFx*NFy*NFz)
+print('Estimate of the number of particles: ', tempN)
+outputFile.write(str(tempN) + '\n0.0000\n')
+
+vm = sqrt(.3)
+def vel():
+    return vm*(2.*ran()-1.)/L
+
+alpha = .45*pi
+beta = .93*pi
+p = [ [ ecc, 0., 0. ] ,
+      [ 0., ecc, 0. ] ]
+wafer, fluid = 0,0
+
+
+# wafer layer
+z = 0.25/L
+for ix in range(NPx):
+    x = (0.1 + .5 + 1.0000000000001*ix)/L
+    for iy in range(NPy):
+        wafer += 1
+        # ipc center
+        y = (0.1 + .5 + 1.0000000000001*iy)/L
+        vx = vy = vz = 0.
+        outputFile.write('W\t' + str(x).ljust(20) + '\t' + str(y).ljust(20) + '\t' + str(z).ljust(20) + '\t')
+        outputFile.write(str(vx).ljust(20) + '\t' + str(vy).ljust(20) + '\t' + str(vz).ljust(20) + '\n')
+        # patch ordering
+        j = 0 if (ix + iy)%2==0 else 1
+        # first patch
+        px = x + p[j][0];    px = absolutePBC(px)
+        py = y + p[j][1];    py = absolutePBC(py)
+        pz = z + p[j][2];    pz = absolutePBC(pz)
+        outputFile.write('P\t' + str(px).ljust(20) + '\t' + str(py).ljust(20) + '\t' + str(pz).ljust(20) + '\t')
+        outputFile.write(str(vx).ljust(20) + '\t' + str(vy).ljust(20) + '\t' + str(vz).ljust(20) + '\n')
+        # second patch
+        px = x - p[j][0];    px = absolutePBC(px)
+        py = y - p[j][1];    py = absolutePBC(py)
+        pz = z - p[j][2];    pz = absolutePBC(pz)
+        outputFile.write('P\t' + str(px).ljust(20) + '\t' + str(py).ljust(20) + '\t' + str(pz).ljust(20) + '\t')
+        outputFile.write(str(vx).ljust(20) + '\t' + str(vy).ljust(20) + '\t' + str(vz).ljust(20) + '\n')
+
+# square lattice above the plane
+
+for iz in range(1, NFz + 1):
+    z = (.5 + iz)*spacing
+    for ix in range(NFx):
+        x = (.5 + ix)*spacing
+        for iy in range(NFy):
+            fluid += 1
+            # ipc center
+            y = (.5 + iy)*spacing
+            vx = vel()
+            vy = vel()
+            vz = vel()
+            outputFile.write('F\t' + str(x).ljust(20) + '\t' + str(y).ljust(20) + '\t' + str(z).ljust(20) + '\t')
+            outputFile.write(str(vx).ljust(20) + '\t' + str(vy).ljust(20) + '\t' + str(vz).ljust(20) + '\n')
+            # first patch
+            px = x + p[j][0];    px = absolutePBC(px)
+            py = y + p[j][1];    py = absolutePBC(py)
+            pz = z + p[j][2];    pz = absolutePBC(pz)
+            outputFile.write('P\t' + str(px).ljust(20) + '\t' + str(py).ljust(20) + '\t' + str(pz).ljust(20) + '\t')
+            outputFile.write(str(vx).ljust(20) + '\t' + str(vy).ljust(20) + '\t' + str(vz).ljust(20) + '\n')
+            # second patch
+            px = x - p[j][0];    px = absolutePBC(px)
+            py = y - p[j][1];    py = absolutePBC(py)
+            pz = z - p[j][2];    pz = absolutePBC(pz)
+            outputFile.write('P\t' + str(px).ljust(20) + '\t' + str(py).ljust(20) + '\t' + str(pz).ljust(20) + '\t')
+            outputFile.write(str(vx).ljust(20) + '\t' + str(vy).ljust(20) + '\t' + str(vz).ljust(20) + '\n')
+
+print('wafer = ', wafer, 'fluid = ', fluid)
+print('tot = ', wafer + fluid)
+print('Resulting density = ', (wafer + fluid)/L**3)
+print('NUMBER OF POINT PARTICLES = ', 3*(wafer + fluid) )
+print('HC radius in reduced units = ', 1/L)
+outputFile.seek(0)
+outputFile.write(str(3*(wafer + fluid)))
