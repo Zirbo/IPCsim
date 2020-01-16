ccf49b4ca091e1b94ed0a5179603016eb097a044
Zirbo <silvano.ferrari@ymail.com>
2020-01-16 15:58:12 +0100

    starting state creator, manner can have noncubic box

--- startingstate_creators/manner_structure.py
+++ startingstate_creators/manner_structure.py
@@ -8,15 +8,17 @@ from numpy.random import ranf as ran
 outputFile = open('startingstate.xyz','w')
 
 Nx = 14            # particles in a side
-Ny = 12            # particles in a side
+Ny = 24            # particles in a side
 Nz = 6             # # of planes
-L  = 12            # scaling (box side)
-N = 3*Nz*(Nx*Ny+int((Nx+1)/2)*int((Ny+1)/2) )
+Lx = 12            # scaling (box side)
+Ly = 24            # scaling (box side)
+Lz = 12            # scaling (box side)
+nIPCs = Nz*(Nx*Ny+int(Nx/2)*int((Ny+4)/2) )
 ecc = 0.22         # eccentricity
-ecc /= L
 cos30 = sqrt(3)*.5
 
-outputFile.write(str(N) + '\n' + str(L) + '\t0.0000\n')
+outputFile.write(str(3*nIPCs) + '\n')
+outputFile.write('Lx = {}    Ly = {}    Lz = {}\n'.format(Lx, Ly, Lz))
 
 vm = sqrt(.3)
 def vel():
@@ -32,12 +34,12 @@ wafer, schokolade = 0,0
 
 for iz in range(Nz):
   # wafer layer
-  z = (.5+2.0000000000001*iz)/L
+  z = .5+2.0000000000001*iz
   for ix in range(Nx):
-    x = (.5+1.0000000000001*cos30*ix)/L
+    x = .5+1.0000000000001*cos30*ix
     for iy in range(Ny):
       j = 0 if (iy+(int((ix+1)/2))%2)%2==0 else 1
-      y = ((.5+1.0000000000001*iy) if ix%2==0 else (1.0000000000001*iy))/L
+      y = (.5+1.0000000000001*iy) if ix%2==0 else (1.0000000000001*iy)
       vx = vel()
       vy = vel()
       vz = vel()
@@ -50,15 +52,16 @@ for iz in range(Nz):
            '\t'+str(z-p[j][2]).ljust(20)+'\t')
       outputFile.write(str(vx).ljust(20)+'\t'+str(vy).ljust(20)+'\t'+str(vz).ljust(20)+'\n')
       wafer += 1
+for iz in range(Nz):
   # chocolate layer
-  z = (1.5+2.0000000000001*iz)/L
+  z = 1.5+2.0000000000001*iz
   for ix in range(0,Nx,2):
-    x = (.5+1.0000000000001*cos30*(ix+0.5))/L
+    x = .5+1.0000000000001*cos30*(ix+0.5)
     #x = (1.+1.0000000000001*(ix+0.5))/L
  #   for iy in range(0,Ny,2):
  #     y = (1.0000000000001*iy)/L
-    for iy in range(0,Ny+2,2):     # golden
-      y = (.8*iy)/L                # golden
+    for iy in range(0,Ny+4,2):     # golden
+      y = 1.6 + 0.8*iy                    # golden
 #    for iy in range(0,Ny,1):       # denser
 #      y = iy/L                     # denser
 #    for iy in range(0,Ny+1,3):       # less denser
@@ -75,6 +78,7 @@ for iz in range(Nz):
       schokolade += 1
 print('wafer =',wafer,'schokolade =',schokolade)
 print('tot =',wafer+schokolade, 'ratio =',schokolade/(wafer+schokolade))
-print('NUMBER OF POINT PARTICLES =',3*(wafer+schokolade) )
-outputFile.seek(0)
-outputFile.write(str(3*(wafer+schokolade)))
+print('NUMBER OF POINT PARTICLES =',nIPCs,"=",wafer+schokolade )
+print('density = ', nIPCs/(Lx*Ly*Lz))
+#outputFile.seek(0)
+#outputFile.write(str(3*(wafer+schokolade)))
